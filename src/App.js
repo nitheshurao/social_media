388d4b498cebfabc8b0adb178caa547c082e43c9
Nitesh <nitesh@Niteshs-MacBook-Air.local>
2020-09-02 11:36:12 +0530

    socila media appa

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        hiiih
+    <p> hhhh</p>
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
